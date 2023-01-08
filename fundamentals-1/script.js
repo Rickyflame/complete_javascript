72795260f2a480aa11e60ae7a6c31092156a9a49
Rickyflame <kimumuricky@gmail.com>
2023-01-08 21:47:50 +0300

    js

---
+++ fundamentals-1/script.js
@@ -0,0 +1,12 @@
+let js = "amazing";
+console.log(40+56+123 - 435);
+
+console.log('Ricky');
+
+let firstName = "Rogue";
+let age = "23";
+let loc = "Kanairo";
+
+console.log(firstName);
+console.log(age);
+console.log(loc);
