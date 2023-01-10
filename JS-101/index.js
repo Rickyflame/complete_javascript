374b61e24a838aa574fe7ddfe2c6c9df06beacf8
Rickyflame <kimumuricky@gmail.com>
2023-01-10 10:22:33 +0300

    js tuts

---
+++ JS-101/index.js
@@ -0,0 +1,27 @@
+//this represents a comment.
+let Fname = "Ricky";
+console.log(name);
+//Rules for names should not be reserved keywords
+//should be meaningful
+//Cannot start with a number, space or hyphen
+//are case-sensitive
+let firstame = 'Rogue'; // string literal
+//to declare multiple variable in one line
+let FirstNae = 'Fast', Lastname = 'hamedi';
+const interestRate = 0.3;
+console.log(interestRate);
+let agre = '30'; // a number literal
+let isApproved = true; //boolean literal
+let firstName = undefined;
+let selectedColor = null;
+//JS is a dynamic language
+let person = {
+    name: 'mosh',
+    age: 30
+};
+//dot notation
+person.name = 'John';
+//Bracket notation
+person['name'] = 'Mary';
+
+console.log(person.name);
