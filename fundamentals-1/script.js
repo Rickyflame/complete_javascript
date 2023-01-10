41fb181b98bc92a8ef7dc595e5256beabb486fa3
Rickyflame <kimumuricky@gmail.com>
2023-01-10 10:13:27 +0300

    js tuts

--- fundamentals-1/script.js
+++ fundamentals-1/script.js
@@ -6,6 +6,9 @@ console.log('Ricky');
 let firstName = "Rogue";
 let age = "23";
 let loc = "Kanairo";
+let country = "Kenya";
+let continent = "Africa";
+let population = "66,865,953";
 
 console.log(firstName);
 console.log(age);
